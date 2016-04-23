72d6196c2b54294dbc9e22d2cc5fad823c599eb8
zharry <zh.harry@yahoo.ca>
2016-04-23 01:23:34 +0000

    Syncing

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require("res/head.php") ?>
+        <!-- Always use double quotes -->
+    </head>
+    <body>
+        <?php require("res/header.php") ?>
+        <a href="www.pornhub.com/">This link is for Jim.</a>
+        <iframe src="http://www.accuweather.com/en/ca/toronto/m5g/weather-forecast/55488"></iframe>
+</html>
---
+++ submission.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php $title = "Crowd Sourced Submission | Aricheck-NG"; require 'res/head.php' ?>
+    </head>
+    <body>
+        <?php require 'res/header.php' ?>
+    </body>
+</html>
